d5f26c1048697566ca35d51efb048e4531445e27
Emanuel Borges <ebbmango@tuta.io>
2023-02-11 12:55:58 -0300

    created pages controller alongside its views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'sobre',   to: 'pages#sobre'
+  get 'areas',   to: 'pages#areas'
+  get 'setores', to: 'pages#setores'
+  get 'time',    to: 'pages#time'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
