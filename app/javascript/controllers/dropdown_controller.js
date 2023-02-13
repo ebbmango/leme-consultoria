ec6b981316bda8eb9b547b57f7a72f1db7b72a44
Emanuel Borges <ebbmango@tuta.io>
2023-02-13 16:52:16 -0300

    connected the navbar's dropdowns to their stimulus controllers

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   connect() {
+    console.log('Hello world!')
   }
 }
