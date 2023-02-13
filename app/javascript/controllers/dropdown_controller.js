1aa799ba2353505f19bcf80868d2e78b9a3b9b45
Emanuel Borges <ebbmango@tuta.io>
2023-02-13 16:57:27 -0300

    added the dropdown's link (btn) and menu to the target list of the stimulus controller

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
+  static targets = [ "dropdown", "menu" ]
+
   connect() {
     console.log('Hello world!')
+
+    const dropdown = this.dropdownTarget
+    const menu = this.menuTarget
+
+    console.log(dropdown)
+    console.log(menu)
   }
 }
