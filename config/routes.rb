0379364f74e340135feaf118b75575967767d25d
Emanuel Borges <ebbmango@tuta.io>
2023-02-13 17:29:47 -0300

    generated rails controllers for 'setores' and 'areas' (with views and actions)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
   get 'setores', to: 'pages#setores'
   get 'time',    to: 'pages#time'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# get 'setores/agronegocio'
+# get 'setores/infraestrutura'
+# get 'setores/construcao_civil'
+# get 'setores/varejo'
+# get 'setores/instituicoes_financeiras'
+# get 'setores/industria'
+# get 'areas/elaboracao_projetos'
+# get 'areas/assistencia_tecnica'
+# get 'areas/consultoria_financas'
+# get 'areas/consultoria_concorrencial'
+# get 'areas/consultoria_regulatoria'
