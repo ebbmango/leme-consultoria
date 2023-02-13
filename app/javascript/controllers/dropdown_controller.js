3b5d5285b33a113a6d7218976e0d079d06e50b6a
Emanuel Borges <ebbmango@tuta.io>
2023-02-13 17:00:01 -0300

    configured adequate expand/collapse behavior for dropdowns on hover

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -5,12 +5,27 @@ export default class extends Controller {
   static targets = [ "dropdown", "menu" ]
 
   connect() {
-    console.log('Hello world!')
+  }
 
+  expand() {
     const dropdown = this.dropdownTarget
     const menu = this.menuTarget
 
-    console.log(dropdown)
-    console.log(menu)
+    dropdown.classList.toggle("show")
+    dropdown.attributes[3].value = "true"
+
+    menu.classList.toggle("show")
+    menu.setAttribute("data-bs-popper", "static")
+  }
+
+  collapse() {
+    const dropdown = this.dropdownTarget
+    const menu = this.menuTarget
+
+    dropdown.classList.toggle("show")
+    dropdown.attributes[3].value = "false"
+
+    menu.classList.toggle("show")
+    menu.removeAttribute("data-bs-popper")
   }
 }
