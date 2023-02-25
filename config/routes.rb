95ccc72891417ce86b0ef942a88305fb5a256ee7
Emanuel Borges <ebbmango@tuta.io>
2023-02-25 15:05:06 +0100

    configured rails behavior for not found pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#not_found', via: :all
+
   root 'pages#home'
 
+  # Quem somos
   get 'sobre', to: 'pages#sobre'
-
-  # get 'areas',                           to: 'pages#areas'
+  # Áreas de Atuação
   get 'areas/elaboracao_projetos',       to: 'areas#elaboracao_projetos',       as: 'elaboracao_projetos'
   get 'areas/assistencia_tecnica',       to: 'areas#assistencia_tecnica',       as: 'assistencia_tecnica'
   get 'areas/consultoria_financas',      to: 'areas#consultoria_financas',      as: 'consultoria_financas'
   get 'areas/consultoria_concorrencial', to: 'areas#consultoria_concorrencial', as: 'consultoria_concorrencial'
   get 'areas/consultoria_regulatoria',   to: 'areas#consultoria_regulatoria',   as: 'consultoria_regulatoria'
-
-  # get 'setores',                          to: 'pages#setores'
+  # Setores Econômicos
   get 'setores/agronegocio',              to: 'setores#agronegocio',              as: 'agronegocio'
   get 'setores/infraestrutura',           to: 'setores#infraestrutura',           as: 'infraestrutura'
   get 'setores/construcao_civil',         to: 'setores#construcao_civil',         as: 'construcao_civil'
   get 'setores/varejo',                   to: 'setores#varejo',                   as: 'varejo'
   get 'setores/instituicoes_financeiras', to: 'setores#instituicoes_financeiras', as: 'instituicoes_financeiras'
   get 'setores/industria',                to: 'setores#industria',                as: 'industria'
-
+  # Nosso time
   get 'time', to: 'pages#time'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
