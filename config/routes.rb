155ed568476ec89c4b97c4cb7f5230b2f17747aa
Emanuel Borges <ebbmango@tuta.io>
2023-02-25 14:50:01 +0100

    removed unnecessary pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
 
   get 'sobre', to: 'pages#sobre'
 
-  get 'areas',                           to: 'pages#areas'
+  # get 'areas',                           to: 'pages#areas'
   get 'areas/elaboracao_projetos',       to: 'areas#elaboracao_projetos',       as: 'elaboracao_projetos'
   get 'areas/assistencia_tecnica',       to: 'areas#assistencia_tecnica',       as: 'assistencia_tecnica'
   get 'areas/consultoria_financas',      to: 'areas#consultoria_financas',      as: 'consultoria_financas'
   get 'areas/consultoria_concorrencial', to: 'areas#consultoria_concorrencial', as: 'consultoria_concorrencial'
   get 'areas/consultoria_regulatoria',   to: 'areas#consultoria_regulatoria',   as: 'consultoria_regulatoria'
 
-  get 'setores',                          to: 'pages#setores'
+  # get 'setores',                          to: 'pages#setores'
   get 'setores/agronegocio',              to: 'setores#agronegocio',              as: 'agronegocio'
   get 'setores/infraestrutura',           to: 'setores#infraestrutura',           as: 'infraestrutura'
   get 'setores/construcao_civil',         to: 'setores#construcao_civil',         as: 'construcao_civil'
